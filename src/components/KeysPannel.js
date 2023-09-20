edfa180fe430c938fb46da1b262696a3548bb06c
Nisar Saeed <dash75549@gmail.com>
2023-09-20 23:53:08 +0500

    Keys maped seperatly for better readability

--- src/components/KeysPannel.js
+++ src/components/KeysPannel.js
@@ -1,12 +1,18 @@
 import Key from "./Key";
-import "./keypannel.css";
-
-export default function KeysPannel({onKeyClick}) {
-  const keyValues = ['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', 'x', '0', 'AC', '=', '/'];
-
+import "./keypannel.css"
+export default function KeysPannel({ onKeyClick }) {
+  const numericKeys = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+  const operatorKeys = ['+', '-', 'x', '/'];
+  const specialKeys = ['AC', '='];
   return (
     <div className="key-panel">
-      {keyValues.map((value, index) => (
+      {numericKeys.map((value, index) => (
+        <Key key={index} value={value} onclick={onKeyClick} />
+      ))}
+      {specialKeys.map((value, index) => (
+        <Key key={index} value={value} onclick={onKeyClick} />
+      ))}
+      {operatorKeys.map((value, index) => (
         <Key key={index} value={value} onclick={onKeyClick} />
       ))}
     </div>
